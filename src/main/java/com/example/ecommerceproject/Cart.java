6897fde67400c8a40d7f683195f624f2da11896a
“Nadia770” <nadia.m.amir@gmail.com>
2021-11-30 12:59:32 +0000

    add annotation to entities

--- src/main/java/com/example/ecommerceproject/Cart.java
+++ src/main/java/com/example/ecommerceproject/Cart.java
@@ -1,11 +1,27 @@
 package com.example.ecommerceproject;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cart")
 public class Cart {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
+  @Column(name = "name")
   private String name;
+  @Column(name = "price")
   private int price;
+  @Column(name = "description")
   private String description;
+  @Column(name = "image")
   private String image;
+  @Column(name = "userId")
   private int userId;
   public int getId() {
     return id;
--- src/main/java/com/example/ecommerceproject/User.java
+++ src/main/java/com/example/ecommerceproject/User.java
@@ -1,9 +1,21 @@
 package com.example.ecommerceproject;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="user")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
+  @Column(name = "username")
   private String username;
+  @Column(name = "password")
   private String password;
 
   public int getId() {
