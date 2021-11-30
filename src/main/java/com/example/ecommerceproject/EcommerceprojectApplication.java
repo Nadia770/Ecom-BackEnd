dea29d48f421277b41b8b1173807770375d8b0cf
“Nadia770” <nadia.m.amir@gmail.com>
2021-11-30 15:54:10 +0000

    create objects for tables

--- src/main/java/com/example/ecommerceproject/EcommerceprojectApplication.java
+++ src/main/java/com/example/ecommerceproject/EcommerceprojectApplication.java
@@ -17,11 +17,54 @@ public class EcommerceprojectApplication implements CommandLineRunner{
 	@Autowired
 	private CustomerRepository customerRepository;
 
+	@Autowired
+	private CartRepository cartRepository;
+
+	@Autowired
+	private ProductRepository productRepository;
+
 	public void run(String...args) throws Exception {
-		this.customerRepository.save(new Customer("Hannah123", "bluerries"));
-		this.customerRepository.save(new Customer("Nadia221", "redlorry"));
-		this.customerRepository.save(new Customer("Tony_5", "yellowlolly"));
+var hannah = new Customer("Hannah123", "bluerries");
+var nadia = new Customer("Nadia221", "redlorry");
+var tony = new Customer("Tony_5", "yellowlolly");
+
+this.customerRepository.save(hannah);
+this.customerRepository.save(nadia);
+this.customerRepository.save(tony);
+
+this.cartRepository.save(new Cart("Pineapple", 5, "Ripe Pinapple",
+"https://images.unsplash.com/photo-1550258987-190a2d41a8ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnJ1aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60/",
+hannah));
+this.cartRepository.save(new Cart("Strawberries", 5, "Ripe Strawberries",
+"https://images.unsplash.com/photo-1601004890684-d8cbf643f5f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJ1aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60"
+,nadia));
+
+this.productRepository.save(new Product(
+					"Pineapple",
+          5,
+          "Ripe Pinapple",
+          "https://images.unsplash.com/photo-1550258987-190a2d41a8ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnJ1aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60/"
+        ));
+this.productRepository.save(new Product(
+          "Strawberries",
+          5,
+        	"Ripe Strawberries",
+          "https://images.unsplash.com/photo-1601004890684-d8cbf643f5f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJ1aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60"
+				));
+this.productRepository.save(new Product(
+          "Oranges",
+          3,
+          "Ripe Oranges",
+          "https://images.unsplash.com/photo-1557800636-894a64c1696f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60"
+				));
+this.productRepository.save(new Product(
+          "Bananas",
+          2,
+          "Ripe Bananas",
+          "https://images.unsplash.com/photo-1528825871115-3581a5387919?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
+				));
 	}
 
+	
 
 }
