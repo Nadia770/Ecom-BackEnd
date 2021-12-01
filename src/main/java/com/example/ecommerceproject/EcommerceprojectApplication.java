8fb2d5594639f84c4649fbe1013648e8532bd720
“Nadia770” <nadia.m.amir@gmail.com>
2021-12-01 15:47:28 +0000

    create more product objects

--- src/main/java/com/example/ecommerceproject/EcommerceprojectApplication.java
+++ src/main/java/com/example/ecommerceproject/EcommerceprojectApplication.java
@@ -49,7 +49,7 @@ this.cartRepository.save(new Cart(1, "Strawberries", 5, "Ripe Strawberries",
 this.productRepository.save(new Product(
 					"Pineapple",
           5,
-          "Ripe Pinapple",
+          "Ripe Pineapple",
           "https://images.unsplash.com/photo-1550258987-190a2d41a8ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZnJ1aXR8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60/"
         ));
 this.productRepository.save(new Product(
@@ -69,6 +69,54 @@ this.productRepository.save(new Product(
           2,
           "Ripe Bananas",
           "https://images.unsplash.com/photo-1528825871115-3581a5387919?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
+				));
+this.productRepository.save(new Product(
+          "Peach",
+          2,
+          "Ripe Peach",
+          "https://images.pexels.com/photos/6157041/pexels-photo-6157041.jpeg?cs=srgb&dl=pexels-laker-6157041.jpg&fm=jpg"
+				));
+this.productRepository.save(new Product(
+          "Grape",
+          1,
+          "Ripe Grape",
+          "https://images.pexels.com/photos/5945854/pexels-photo-5945854.jpeg?cs=srgb&dl=pexels-any-lane-5945854.jpg&fm=jpg"
+				));
+this.productRepository.save(new Product(
+          "Lemon",
+          1,
+          "Ripe Lemon",
+          "https://images.pexels.com/photos/7657204/pexels-photo-7657204.jpeg?cs=srgb&dl=pexels-cup-of-couple-7657204.jpg&fm=jpg"
+				));
+this.productRepository.save(new Product(
+          "Kiwi",
+          1,
+          "Ripe Kiwi",
+          "https://images.pexels.com/photos/6156993/pexels-photo-6156993.jpeg?cs=srgb&dl=pexels-laker-6156993.jpg&fm=jpg"
+				));
+this.productRepository.save(new Product(
+          "Grape Fruit",
+          1,
+          "Ripe Grape Fruit",
+          "https://images.pexels.com/photos/7195269/pexels-photo-7195269.jpeg?cs=srgb&dl=pexels-shvets-production-7195269.jpg&fm=jpg"
+				));
+this.productRepository.save(new Product(
+          "Papaya",
+          1,
+          "Ripe Papaya",
+          "https://images.pexels.com/photos/5945731/pexels-photo-5945731.jpeg?cs=srgb&dl=pexels-any-lane-5945731.jpg&fm=jpg"
+				));
+this.productRepository.save(new Product(
+          "Avocado",
+          1,
+          "Ripe Avocado",
+          "https://images.pexels.com/photos/5945769/pexels-photo-5945769.jpeg?cs=srgb&dl=pexels-any-lane-5945769.jpg&fm=jpg"
+				));
+	this.productRepository.save(new Product(
+          "Melon",
+          1,
+          "Ripe Melon",
+          "https://images.pexels.com/photos/7065188/pexels-photo-7065188.jpeg?cs=srgb&dl=pexels-laura-tancredi-7065188.jpg&fm=jpg"
 				));
 	}
 
